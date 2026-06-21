f04c01f96daf6ec0f466c2f3b78c708f9696a00e
vitorsome <97389137+vitorsome@users.noreply.github.com>
2026-06-21 09:32:30 -0300

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(position)
+        tuples = []
+        for i in range(n):
+            tuples.append((position[i], speed[i]))
+        tuples.sort(reverse=True)
+        fleets = 0
+        curr_time = 0
+        for i in range(n):
+            time = (target - tuples[i][0]) / tuples[i][1]
+            if time <= curr_time:
+                curr_time = max(time, curr_time)
+            else:
+                fleets += 1
+                curr_time = time
+        return fleets
